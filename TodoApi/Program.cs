778fe4fe7ad74532ed6de59d09177f6886477681
6187144 <6187144@cegep-heritage.qc.ca>
2022-04-20 13:35:39 -0400

    lab2

---
+++ TodoApi/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var dbmsVersion = new MariaDbServerVersion(builder.Configuration.GetValue<string>("DBMSVersion"));
+var connString = builder.Configuration.GetConnectionString("TodoDbContext");
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoDbContext>(opt => opt.UseMySql(connString, dbmsVersion));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
